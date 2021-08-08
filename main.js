10f8b2cfdc47c17ce966fa662eba266eff325407
Everett Blakley <everettblakley@gmail.com>
2021-08-08 12:35:47 -0600

    Adding tailwind and working on more slides

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-import Reveal from 'reveal.js';
+import Reveal from "reveal.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm";
+import Notes from "reveal.js/plugin/notes/notes.esm";
 
 let deck = new Reveal({
-  width: 1920,
-  height: 1080,
+  plugins: [Highlight, Notes]
 });
 
-deck.initialize();
+deck.initialize({
+  hash: true
+});
 
 deck.getSlides().forEach((slide) => {
-  slide.setAttribute('data-background-image', 'background.png')
-})
+  slide.setAttribute("data-background-image", "background.png");
+});
